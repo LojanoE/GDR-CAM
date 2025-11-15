26a27c7d11ec20815a667f5273da788f82af9c45
LojanoE <estanislaolojano@gmail.com>
2025-11-14 21:58:41 -0500

    mian
    frntes anteriores

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE_NAME = 'gdr-cam-v68.6.2';
-const STATIC_CACHE_NAME = 'gdr-cam-static-v68.6.2';
-const RUNTIME_CACHE_NAME = 'gdr-cam-runtime-v68.6.2';
+const CACHE_NAME = 'gdr-cam-v68.6.3';
+const STATIC_CACHE_NAME = 'gdr-cam-static-v68.6.3';
+const RUNTIME_CACHE_NAME = 'gdr-cam-runtime-v68.6.3';
 
 const urlsToCache = [
   './',
