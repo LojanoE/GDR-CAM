dd90cfb051ee2f9e58e191eb34392a34b2c0fe1b
kathyrodasv <rodaskathy21@gmail.com>
2025-11-27 16:00:26 -0500

    Main
    Dren D-11

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE_NAME = 'gdr-cam-v68.6.7';
-const STATIC_CACHE_NAME = 'gdr-cam-static-v68.6.7';
-const RUNTIME_CACHE_NAME = 'gdr-cam-runtime-v68.6.7';
+const CACHE_NAME = 'gdr-cam-v68.6.8';
+const STATIC_CACHE_NAME = 'gdr-cam-static-v68.6.8';
+const RUNTIME_CACHE_NAME = 'gdr-cam-runtime-v68.6.8';
 
 const urlsToCache = [
   './',
